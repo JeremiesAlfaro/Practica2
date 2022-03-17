6f00eab535032cdf3da0abae768da358cc0e4965
JeremiesAlfaro <a01282692@tec.mx>
2022-03-16 18:10:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ var app = new Vue({
 var app22 = new vue({
     el: '#app22',
     data:{
-        imag: 'Sunset.png'
+        imag: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/e0/ce/85/sunset-beach.jpg?w=1200&h=-1&s=1'
     }
 })
